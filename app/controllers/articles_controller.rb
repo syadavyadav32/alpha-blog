8159562a78be10e5b464e72a126f88d03031fd46
Shubham <syadav.yadav32@gmail.com>
2018-01-08 19:40:08 +0000

    create show, add error messages and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,21 @@ def new
 end
 
 def create
+  #render plain: params[:article].inspect  -- to show the parameters passed to create action from new.html.erb file 
+  
   @article = Article.new(article_params)
-  @article.save
-  redirect_to article_path(@article) 
+  if @article.save
+    flash[:notice] = "Article was successfully created"
+    redirect_to article_path(@article) 
+  else
+    render 'new'  #render new template which is new.hrml.erb
+  end
 end
+
+def show
+  @article = Article.find(params[:id])
+end
+  
 
 private
 
